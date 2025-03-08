398ee316aa874c7fdadd6756fc7102e455c83f57
Lora Varagic <l.varagic.10@gmail.com>
2025-03-08 13:36:03 +1000

    Milestone 1- Implemented 'Add New ToDo' button with opacity change

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing Git</Text>
+      <Text>My To-Do List</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add New Todo</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3F3F4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  buttonContainer: {
+    bottom: 40,
+    alignItems: "center",
+    position: "absolute",
+  },
+
+  button: {
+    backgroundColor: "#814DF6",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 7,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
+
